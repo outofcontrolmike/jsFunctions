af64ceffce12f6d1d9504b3269207f243cb1e122
WILSON <mw0415436@otc.edu>
2018-09-05 11:24:53 -0500

    square

---
+++ Functions.js
@@ -0,0 +1,11 @@
+// square the given number x
+function square1(x)  {
+
+}
+
+// Square the given number x
+const square2 = x => // 
+
+console.log(square1(0));
+console.log(square1(2));
+console.log(square1(5));
