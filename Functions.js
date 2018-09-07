b5615e7059c97c952b8c7b783f32e62d23d28070
WILSON <mw0415436@otc.edu>
2018-09-07 10:37:57 -0500

    updated squaring program

--- Functions.js
+++ Functions.js
@@ -1,17 +1,32 @@
-// Square the following number x
+/* Square the following number x
 function square1(x)
 {
-    return x;
+    return x * x;
 }
 
-// Square the given number x
-const sqaure2 = x =>
-{   
-    console.log(sqaure1(0));
+
+*/
+
+// Square the given number x //
+function square1(x) {
+return x = x * x;
+} 
+
+function square2(x) {
+return x = x * x;
+
+}
+    console.log(square1(0));
     console.log(square1(2));
     console.log(square1(5));
 
     console.log(square2(0));
     console.log(square2(2));
-    console.log(sqaure2(5));
-}
+    console.log(square2(5));
+
+for ( let i = 0; i <=10; i++)
+    {
+        console.log(square1(i));
+        console.log(square2(i));
+    }
+    
